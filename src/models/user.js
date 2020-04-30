1cc6c88d8a4adb690e27141d95829be9932cb1cf
Hannah Barton <hannbarton@gmail.com>
2020-04-30 16:10:32 -0400

    add salting password for security

--- src/models/user.js
+++ src/models/user.js
@@ -22,3 +22,32 @@ const User = db.define('user', {
   },
 });
 
+User.prototype.correctPassword = function (userPwd) {
+  return User.encryptPassword(userPwd, this.salt()) === this.password();
+};
+
+User.generateSalt = function () {
+  return crypto.randomBytes(16).toString('base64');
+};
+
+User.encryptPassword = function (plainText, salt) {
+  return crypto
+    .createHash('RSA-SHA256')
+    .update(plainText)
+    .update(salt)
+    .digest('hex');
+};
+
+const setSaltAndPassword = (user) => {
+  const usr = user;
+  if (usr.changed('password')) {
+    usr.salt = User.generateSalt();
+    usr.password = User.encryptPassword(user.password(), user.salt());
+  }
+  return usr;
+};
+
+User.beforeCreate(setSaltAndPassword);
+User.beforeUpdate(setSaltAndPassword);
+
+module.exports = User;
