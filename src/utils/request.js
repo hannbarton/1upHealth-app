8420e680ce295e3e730d483d2c5763e6d3c95b14
Hannah Barton <hannbarton@gmail.com>
2020-04-30 15:06:34 -0400

    add request promise util to make external api requests

---
+++ src/utils/request.js
@@ -0,0 +1,27 @@
+const rp = require('request-promise');
+const _ = require('lodash');
+
+
+const request = (method, resource, options = '') => {
+  const uri = `${process.env.BASE_URL}/${resource}`;
+  const headers = {
+    Authorization: `Bearer ${process.env.ACCESS_BEARER_TOKEN}`,
+  };
+
+  const opts = _.merge({
+    method,
+    uri,
+    json: true,
+    headers,
+  }, options);
+
+  return rp(opts)
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((err) => {
+      console.log(`There was an error making the api request: ${err}`, method, uri, options);
+    });
+};
+
+module.exports = request;
