51b9c6c9678e21d209258ed0d574b0175a0c900e
Hannah Barton <hannbarton@gmail.com>
2020-04-30 20:59:49 -0400

    moving file structure

---
+++ client/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  // eslint-disable-next-line
+  <div>this app is working</div>, document.getElementById('app'),
+);
