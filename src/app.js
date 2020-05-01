f13c9221ee2db786eb5e5fbb1205e6e83a0b0e3f
Hannah Barton <hannbarton@gmail.com>
2020-04-30 21:58:02 -0400

    adding mandatory pg for sequelize

--- src/app.js
+++ src/app.js
@@ -19,19 +19,15 @@ const createApp = () => {
 
   app.use(express.static(path.join(__dirname, '..', 'public')));
 
-  app.use('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'public/index.html'));
-  });
-
-  app.use((req, res, next) => {
-    if (path.extname(req.path).length) {
-      const err = new Error('Not found');
-      err.status = 404;
-      next(err);
-    } else {
-      next();
-    }
-  });
+  // app.use((req, res, next) => {
+  //   if (path.extname(req.path).length) {
+  //     const err = new Error('Not found');
+  //     err.status = 404;
+  //     next(err);
+  //   } else {
+  //     next();
+  //   }
+  // });
 
   app.get('/login', (req, res, next) => {
     try {
@@ -41,6 +37,10 @@ const createApp = () => {
     }
   });
   // app.get('/login', authenticate);
+
+  app.use('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'public/index.html'));
+  });
 
   // error handling endware
   app.use((err, req, res) => {
