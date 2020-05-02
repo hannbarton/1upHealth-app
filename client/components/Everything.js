bb7b7b8a3cbc28e6192f1ea506204194d9ad1e1d
Hannah Barton <hannbarton@gmail.com>
2020-05-02 07:33:26 -0400

    reformatting with modified eslint rules

---
+++ client/components/Everything.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Everything extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+    };
+  }
+}
+
+export default Everything;
--- client/routes.js
+++ client/routes.js
@@ -3,17 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LoginSignup from './components/LoginSignup';
 import Home from './components/Home';
 
-class Routes extends React.Component {
-	render() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={LoginSignup} />
-          <Route path="/home" component={Home} />
-        </Switch>
-      </BrowserRouter>
-    );
-  }
-}
+const Routes = () => (
+  <BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={LoginSignup} />
+      <Route path="/home" component={Home} />
+    </Switch>
+  </BrowserRouter>
+);
+
 
 export default Routes;
